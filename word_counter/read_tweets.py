5e62c9228062679fb23213a23a127e89bbd56cc4
Sergio <you@example.com>
2019-05-28 18:02:37 +0200

    Global Vars

--- word_counter/read_tweets.py
+++ word_counter/read_tweets.py
@@ -1,10 +1,11 @@
 
 import tweepy
 from datetime import *
-consumer_key = "E9Z9R0YUb6MuoWQlInGqESQ9y"
-consumer_secret = "muEAST4MncsPGFp7g6pJXeoOdURO9cIHquN9Yg6V7O78S16RF1"
-access_token ="1092865494263181312-7FlipxBhpTcBTIFsCKXpn9ZKVBucJt"
-access_token_secret ="slaAYc6CZTkSQwyUzuTxs5rZcFRdhzR188lOwCrnc8lcf"
+import os
+consumer_key = os.environ["CONSUMER_KEY"]
+consumer_secret = os.environ["CONSUMER_SECRET"]
+access_token = os.environ["ACCESS_TOKEN"]
+access_token_secret = os.environ["ACCESS_TOKEN_SECRET"]
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
